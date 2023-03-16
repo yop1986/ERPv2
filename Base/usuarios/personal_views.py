fab5d5607b1d847c38532ae517cbce2e9ca45da4
CART Godoy <pablo.godoy@banrural.com.gt>
2023-03-16 13:01:47 -0600

    feat(personal_views): se agregan clases view personalizadas

--- Base/usuarios/personal_views.py
+++ Base/usuarios/personal_views.py
@@ -1,11 +1,18 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 
-from django.views.generic.edit import FormView, UpdateView
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import FormView, UpdateView, CreateView
 from django.views.generic.list import ListView
 
 
+class PersonalTemplateView(LoginRequiredMixin, PermissionRequiredMixin, SuccessMessageMixin, TemplateView):
+    pass 
+    
 class PersonalFormView(LoginRequiredMixin, PermissionRequiredMixin, SuccessMessageMixin, FormView):
+    pass
+
+class PersonalCreateView(LoginRequiredMixin, PermissionRequiredMixin, SuccessMessageMixin, CreateView):
     pass
 
 class PersonalUpdateView(LoginRequiredMixin, PermissionRequiredMixin, SuccessMessageMixin, UpdateView):
