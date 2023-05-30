21a8fd02066d74f3a817f6a9a2f8a827cfe5c0b1
Pablo Godoy <pablodavid36@gmail.com>
2023-05-30 11:05:37 -0600

    usuarios (refactor): ContextMixin para aprovechar herencia

--- Base/usuarios/personal_views.py
+++ Base/usuarios/personal_views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, ContextMixin
 from django.views.generic.edit import FormView, UpdateView, CreateView, DeleteView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
@@ -10,45 +10,29 @@ from .app_funciones import Configuraciones
 
 gConfiguracion = Configuraciones()
 
+class PersonalContextMixin(ContextMixin):
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['general'] = {'nombre_sitio': gConfiguracion.get_value('sitio', 'nombre')}
+        return context 
 
-class PersonalTemplateView(LoginRequiredMixin, PermissionRequiredMixin, SuccessMessageMixin, TemplateView):
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-        context['general'] = {'nombre_sitio': gConfiguracion.get_value('sitio', 'nombre')}
-        return context
-    
-class PersonalFormView(LoginRequiredMixin, PermissionRequiredMixin, SuccessMessageMixin, FormView):
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-        context['general'] = {'nombre_sitio': gConfiguracion.get_value('sitio', 'nombre')}
-        return context
+class PersonalTemplateView(LoginRequiredMixin, PermissionRequiredMixin, SuccessMessageMixin, PersonalContextMixin, TemplateView):
+    pass
 
-class PersonalCreateView(LoginRequiredMixin, PermissionRequiredMixin, SuccessMessageMixin, CreateView):
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-        context['general'] = {'nombre_sitio': gConfiguracion.get_value('sitio', 'nombre')}
-        return context
+class PersonalFormView(LoginRequiredMixin, PermissionRequiredMixin, SuccessMessageMixin, PersonalContextMixin, FormView):
+    pass
 
-class PersonalUpdateView(LoginRequiredMixin, PermissionRequiredMixin, SuccessMessageMixin, UpdateView):
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-        context['general'] = {'nombre_sitio': gConfiguracion.get_value('sitio', 'nombre')}
-        return context
+class PersonalCreateView(LoginRequiredMixin, PermissionRequiredMixin, SuccessMessageMixin, PersonalContextMixin, CreateView):
+    pass
 
-class PersonalListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-        context['general'] = {'nombre_sitio': gConfiguracion.get_value('sitio', 'nombre')}
-        return context
+class PersonalUpdateView(LoginRequiredMixin, PermissionRequiredMixin, SuccessMessageMixin, PersonalContextMixin, UpdateView):
+    pass
 
-class PersonalDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-        context['general'] = {'nombre_sitio': gConfiguracion.get_value('sitio', 'nombre')}
-        return context
+class PersonalListView(LoginRequiredMixin, PermissionRequiredMixin, PersonalContextMixin, ListView):
+    pass
 
-class PersonalDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-        context['general'] = {'nombre_sitio': gConfiguracion.get_value('sitio', 'nombre')}
-        return context
+class PersonalDetailView(LoginRequiredMixin, PermissionRequiredMixin, PersonalContextMixin, DetailView):
+    pass
+
+class PersonalDeleteView(LoginRequiredMixin, PermissionRequiredMixin, PersonalContextMixin, DeleteView):
+    pass
